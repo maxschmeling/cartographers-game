862d96a93329bb3462c685303441e9b6f350f975
Max Schmeling <max.schmeling@automattic.com>
2023-10-13 20:03:49 -0500

    Automatically add/remove coins when surrounding a mountain

--- app/game/Coin.tsx
+++ app/game/Coin.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
-export default function Coin() {
-	const [ selected, setSelected ] = useState<boolean>( false );
+type Props = {
+	selected: boolean;
+	onClick: () => void;
+}
 
+const Coin: React.FC<Props> = ( { selected, onClick } ) => {
 	return (
 		<button
 			className={ [ styles.coin, selected && styles.selected ].join( ' ' ) }
-			onClick={ () => setSelected( !selected ) }
-		></button>
+			onClick={ onClick }
+		/>
 	);
 }
+
+export default Coin
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import styles from './page.module.css'
 import { TileType } from './tiles';
 import MountainTile from './tiles/MountainTile';
@@ -9,7 +9,7 @@ import StandardTile from './tiles/StandardTile';
 import Coin from './Coin';
 import SeasonScore from './SeasonScore';
 
-const ruins = [
+const ruins: Array<[ number, number ]> = [
 	[ 5, 1 ],
 	[ 1, 2 ],
 	[ 9, 2 ],
@@ -17,7 +17,7 @@ const ruins = [
 	[ 9, 8 ],
 	[ 1, 8 ]
 ];
-const mountains = [
+const mountains: Array<[ number, number ]> = [
 	[ 3, 1 ],
 	[ 2, 8 ],
 	[ 5, 5 ],
@@ -25,9 +25,126 @@ const mountains = [
 	[ 7, 9 ],
 ];
 
+type SeasonScore = {
+	edictOne: number;
+	edictTwo: number;
+	coinCount: number;
+	monsterCount: number;
+}
+
+type TileSelection = {
+	row: number;
+	column: number;
+	type: TileType;
+}
+
+type GameBoard = {
+	selections: TileSelection[];
+	coinCount: number;
+	seasonScores: SeasonScore[];
+}
+
+function loadOrInitGame(): GameBoard {
+	const board = localStorage.getItem( 'board' );
+
+	if ( board ) {
+		return JSON.parse( board );
+	}
+
+	return {
+		selections: [],
+		coinCount: 0,
+		seasonScores: [
+			{ edictOne: 0, edictTwo: 0, coinCount: 0, monsterCount: 0 },
+			{ edictOne: 0, edictTwo: 0, coinCount: 0, monsterCount: 0 },
+			{ edictOne: 0, edictTwo: 0, coinCount: 0, monsterCount: 0 },
+			{ edictOne: 0, edictTwo: 0, coinCount: 0, monsterCount: 0 },
+		]
+	};
+}
+
+type GameBoardAction = 
+	| { type: 'select-tile', row: number, column: number, tileType: TileType }
+	| { type: 'clear-tile', row: number, column: number }
+	| { type: 'set-season-score', season: number, score: SeasonScore }
+	| { type: 'set-coin-count', coinCount: number }
+	| { type: 'clear-board' };
+
+function findAdjacentMountain( row: number, column: number ): [ number, number ] | null {
+	const adjacentMountains = mountains.filter( mountain => {
+		const rowDiff = Math.abs( mountain[ 1 ] - row );
+		const columnDiff = Math.abs( mountain[ 0 ] - column );
+
+		return ( rowDiff === 0 && columnDiff === 1 ) || ( rowDiff === 1 && columnDiff === 0 );
+	} );
+
+	if ( adjacentMountains.length ) {
+		return adjacentMountains[ 0 ];
+	}
+
+	return null;
+}
+
+function mountainIsSurrounded( selections: TileSelection[], mountain: [ number, number ] ): boolean {
+	const adjacentTiles = [
+		[ mountain[ 0 ] - 1, mountain[ 1 ] ],
+		[ mountain[ 0 ] + 1, mountain[ 1 ] ],
+		[ mountain[ 0 ], mountain[ 1 ] - 1 ],
+		[ mountain[ 0 ], mountain[ 1 ] + 1 ],
+	];
+
+	return adjacentTiles.every( tile => selections.some( s => s.row === tile[ 1 ] && s.column === tile[ 0 ] ) );
+}
+
+function gameReducer( state: GameBoard, action: GameBoardAction ): GameBoard {
+	let newBoard = { ...state };
+
+	switch ( action.type ) {
+		case 'select-tile':
+			newBoard.selections = newBoard.selections.filter( s => s.row !== action.row || s.column !== action.column );
+			newBoard.selections = [ ...newBoard.selections, { row: action.row, column: action.column, type: action.tileType } ];
+
+			const selectTileAdjacentMountain = findAdjacentMountain( action.row, action.column );
+			
+			if ( selectTileAdjacentMountain && mountainIsSurrounded( newBoard.selections, selectTileAdjacentMountain ) ) {
+				newBoard.coinCount++;
+			}
+
+			break;
+		case 'clear-tile':
+			newBoard.selections = newBoard.selections.filter( s => s.row !== action.row || s.column !== action.column );
+
+			const clearTileAdjacentMountain = findAdjacentMountain( action.row, action.column );
+
+			if (
+				clearTileAdjacentMountain &&
+				mountainIsSurrounded( state.selections, clearTileAdjacentMountain ) && 
+				! mountainIsSurrounded( newBoard.selections, clearTileAdjacentMountain )
+			) {
+				newBoard.coinCount--;
+			}
+
+			break;
+		case 'set-season-score':
+			newBoard.seasonScores[ action.season ] = action.score;
+			break;
+		case 'set-coin-count':
+			newBoard.coinCount = action.coinCount;
+			break;
+		case 'clear-board':
+			localStorage.removeItem( 'board' );
+			newBoard = loadOrInitGame();
+			break;
+	}
+
+	localStorage.setItem( 'board', JSON.stringify( newBoard ) );
+	
+	return newBoard;
+}
+
 export default function Game() {
 	const [ brush, setBrush ] = useState<TileType>( 'village' );
-	const [ board, setBoard ] = useState< { row: number, column: number, type: TileType }[] >( [ ] );
+	const [ board, dispatch ] = useReducer( gameReducer, undefined, loadOrInitGame );
 	const [ seasonOneScore, setSeasonOneScore ] = useState( 0 );
 	const [ seasonTwoScore, setSeasonTwoScore ] = useState( 0 );
 	const [ seasonThreeScore, setSeasonThreeScore ] = useState( 0 );
@@ -35,29 +152,45 @@ export default function Game() {
 
 	const gameBoard = [];
 
-	function toggleTile( row: number, column: number ) {
-		const type = board.find( s => s.row === row && s.column === column )?.type ?? null;
-
-		if ( type ) {
-			setBoard( board.filter( s => s.row !== row || s.column !== column ) );
-		} else {
-			setBoard( [ ...board, { row, column, type: brush } ] );
-		}
-	}
-
 	for ( let row = 0; row < 11; row++ ) {
 		for ( let column = 0; column < 11; column++ ) {
 			const key = `${ row }x${ column }`;
 
-			const type = board.find( s => s.row === row && s.column === column )?.type ?? null;
+			const type = board.selections.find( s => s.row === row && s.column === column )?.type ?? null;
 
 			if ( ruins.some( ruin => ruin[ 0 ] === column && ruin[ 1 ] === row ) ) {
-				gameBoard.push( <RuinTile key={ key } row={ row } column={ column } type={ type } onClick={ toggleTile } /> );
+				gameBoard.push(
+					<RuinTile 
+						key={ key }
+						row={ row }
+						column={ column }
+						type={ type }
+						onClick={ toggleTile }
+					/>
+				);
 			} else if ( mountains.some( mountain => mountain[ 0 ] === column && mountain[ 1 ] === row ) ) {
 				gameBoard.push( <MountainTile key={ key } /> );
 			} else {
-				gameBoard.push( <StandardTile key={ key } row={ row } column={ column } type={ type } onClick={ toggleTile } /> );
+				gameBoard.push(
+					<StandardTile
+						key={ key }
+						row={ row }
+						column={ column }
+						type={ type }
+						onClick={ toggleTile }
+					/>
+				);
 			}
+		}
+	}
+
+	function toggleTile( row: number, column: number ) {
+		const tile = board.selections.find( s => s.row === row && s.column === column );
+
+		if ( ! tile || tile.type !== brush ) {
+			dispatch( { type: 'select-tile', row, column, tileType: brush } );
+		} else {
+			dispatch( { type: 'clear-tile', row, column } );
 		}
 	}
 
@@ -74,7 +207,7 @@ export default function Game() {
 						<input type="text" />
 					</div>
 				</div>
-				<button onClick={ () => setBoard( [] ) }>Clear</button>
+				<button onClick={ () => dispatch( { type: 'clear-board' } ) }>Clear</button>
 			</header>
 			<main>
 				<div className={ styles.tileBoard }>
@@ -88,20 +221,19 @@ export default function Game() {
 					<button className={ styles.monster } onClick={ () => setBrush( "monster" ) }>Monster</button>
 				</div>
 				<div className={ styles.coins }>
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
-					<Coin />
+					{ [ ...Array( 14 ) ].map(
+							( _, index ) => (
+								<Coin key={ index } selected={ board.coinCount > index } onClick={
+									() => {
+										if ( board.coinCount === index + 1 ) {
+											dispatch( { type: "set-coin-count", coinCount: index } );
+										} else {
+											dispatch( { type: "set-coin-count", coinCount: index + 1 } );
+										}
+									}
+								} />
+							)
+						) }
 				</div>
 				<div className={ styles.scoreBoard }>
 					<div className={ styles.seasonScores }>
