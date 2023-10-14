d00cbfadbf37fe41ec5bd82c553b2ce3a2ff9478
Max Schmeling <max.schmeling@automattic.com>
2023-10-13 20:09:46 -0500

    Add og tags

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="A web based Cartographers game board." />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:title" content="Cartographers - Schmeling Family" />
+      <meta property="og:description" content="A web based Cartographers game board." />
+      <meta property="og:image" content="/ogimage.png" />
     </>
   )
 }
